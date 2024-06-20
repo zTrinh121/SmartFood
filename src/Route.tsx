8339d286219572da0fd9ae9cf92066e75d7828eb
Trinh Nguyen <137372274+zTrinh121@users.noreply.github.com>
2024-06-21 00:16:58 +0700

    route

--- src/Route.tsx
+++ src/Route.tsx
@@ -1,14 +1,16 @@
-import { Routes as RouterRoutes, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom
 import { Home, CookingYourself, DinningOut, Login } from './pages';
 
 const Routes = () => {
   return (
-    <RouterRoutes>
-      <Route path="/" element={<Home />} />
+    <BrowserRouter>
+    <Switch>
+    <Route path="/" element={<Home />} />
       <Route path="/cooking" element={<CookingYourself />} />
       <Route path="/dining" element={<DinningOut />} />
       <Route path="/login" element={<Login />} />
-    </RouterRoutes>
+    </Switch>
+    </BrowserRouter>
   );
 };
 
